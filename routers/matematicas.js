a72777ab7c950643cbc6c63628a990d4d6c837af
Alvaro Barrero <barreroalvaro2007@gmail.com>
2023-02-11 20:11:02 +0100

    cleaning the code

--- routers/matematicas.js
+++ routers/matematicas.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const {matematicas} = require("../datos/cursos.js").cursos;
+const { matematicas } = require("../datos/cursos.js").cursos;
 
 // router
 const routerMatematicas = express.Router();
@@ -11,34 +11,34 @@ routerMatematicas.use(express.json());
 
 routerMatematicas.get("/", (req, res) => {
   let cursosMatematicas = JSON.stringify(matematicas);
-  res.send(cursosMatematicas);
+  res.status(200).send(cursosMatematicas);
 });
-  
+
 routerMatematicas.get("/:tema", (req, res) => {
   const tema = req.params.tema;
   let cursoMatematicas = matematicas.filter(curso => curso.tema == tema);
-  if (cursoMatematicas.length === 0){
+  if (cursoMatematicas.length === 0) {
     return res.status(404).send(`${res.statusCode}: No se ha encontrado nada en ${req.url}`);
   }
   cursoMatematicas = JSON.stringify(cursoMatematicas);
-  res.send(cursoMatematicas);
+  res.status(200).send(cursoMatematicas);
 });
-  
+
 routerMatematicas.get("/:tema/:nivel", (req, res) => {
-const {tema} = req.params;
-const {nivel} = req.params;
-let cursoMatematicas = [];
-if (tema == "*"){
-  cursoMatematicas = matematicas.filter(curso => curso.nivel == nivel);
-}
-else {
-  cursoMatematicas = matematicas.filter(curso => curso.tema == tema && curso.nivel == nivel);
-}
-if (cursoMatematicas.length === 0){
-  return res.status(404).send(`${res.statusCode}: No se han encontrado cursos de matematicas del tema ${tema} con un nivel de fificultad ${nivel}`);
-}
-cursoMatematicas = JSON.stringify(cursoMatematicas);
-return res.send(cursoMatematicas);
+  const { tema } = req.params;
+  const { nivel } = req.params;
+  let cursoMatematicas = [];
+  if (tema == "*") {
+    cursoMatematicas = matematicas.filter(curso => curso.nivel == nivel);
+  }
+  else {
+    cursoMatematicas = matematicas.filter(curso => curso.tema == tema && curso.nivel == nivel);
+  }
+  if (cursoMatematicas.length === 0) {
+    return res.status(404).send(`${res.statusCode}: No se han encontrado cursos de matematicas del tema ${tema} con un nivel de fificultad ${nivel}`);
+  }
+  cursoMatematicas = JSON.stringify(cursoMatematicas);
+  return res.status(200).send(cursoMatematicas);
 });
 
 
